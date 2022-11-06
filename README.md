a218c2fe1c5d6b21ea02106d2bd4242929cbf656
Angelo <angeloechaves@gmail.com>
2022-11-05 21:35:11 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CohostKit
+
+A description of this package.
---
+++ Tests/CohostKitTests/CohostKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CohostKit
+
+final class CohostKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CohostKit().text, "Hello, World!")
+    }
+}
